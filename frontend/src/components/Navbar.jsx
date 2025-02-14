1f98b148465ba8cf9922e4492a704ae64bce2764
Rishita77 <rishita.m8@gmail.com>
2025-02-14 20:58:00 +0530

    logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,12 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <h2>ColdDigger</h2>
+      <div className="navbar-brand">
+        <div className="brand-content">
+          <img src="/favicon.png" alt="ColdDigger Logo" className="navbar-logo" />
+          <h2 className="brand-name">ColdDigger</h2>
+        </div>
+      </div>
       <ul>
         <li>
           <Link to="/">Home</Link>
